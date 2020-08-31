04a76fadb35060425cd0ccec4cfdd3eb652dca53
hamsoace <hamsoace@gmail.com>
2020-08-31 13:49:38 +0300

    display functionality on grid

--- app.js
+++ app.js
@@ -2,61 +2,68 @@ document.addEventListener('DOMContentLoader', () =>{
     const cardArray = [
         {
             name: 'fries',
-            img: 'images/fries.png'
+            img: '/images/fries.png'
         },
         {
             name: 'fries',
-            img: 'images/fries.png'
+            img: '/images/fries.png'
         },
         {
             name: 'cheeseburger',
-            img: 'images/cheeseburger.png'
+            img: '/images/cheeseburger.png'
         },
         {
             name: 'cheeseburger',
-            img: 'images/cheeseburger.png'
+            img: '/images/cheeseburger.png'
         },
         {
             name: 'hotdog',
-            img: 'images/hotdog.png'
+            img: '/images/hotdog.png'
         },
         {
             name: 'hotdog',
-            img: 'images/hotdog.png'
+            img: '/images/hotdog.png'
         },
         {
             name: 'ice-cream',
-            img: 'images/ice-cream.png'
+            img: '/images/ice-cream.png'
         },
         {
             name: 'ice-cream',
-            img: 'images/ice-cream.png'
+            img: '/images/ice-cream.png'
         },
         {
             name: 'milkshake',
-            img: 'images/milkshake.png'
+            img: '/images/milkshake.png'
         },
         {
             name: 'milkshake',
-            img: 'images/milkshake.png'
+            img: '/images/milkshake.png'
         },
         {
             name: 'pizza',
-            img: 'images/pizza.png'
+            img: '/images/pizza.png'
         },
         {
             name: 'pizza',
-            img: 'images/pizza.png'
+            img: '/images/pizza.png'
         },
-    ]
+    ];
 
-    const grid = document.querySelector('.grid')
+    const grid = document.querySelector(".grid");
 
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
-            var card = document.createElement('img')
-            card.setAttribute('src', 'images/blank.png')
+            var card = document.createElement('img');
+            card.setAttribute('src', '/images/blank.png');
+            card.setAttribute('data-id', i);
+            //card.addEventListener('click', flipCard)
+            grid.appendChild(card);
             
         }
     }
-})
+
+    function flipCard() {
+        
+    }
+});
